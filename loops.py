0eaab0dcc950bf63a9993b22e2ccc59ef32df6ed
Taqi Hamoda <taqihisham@gmail.com>
2021-03-06 20:55:29 -0500

    Added code

---
+++ loops.py
@@ -0,0 +1,34 @@
+# DO NOT WRITE THIS
+age = input("How old you are you? ")
+age = int(age)  # age is 5
+
+if age < 0:
+    print("You cannot be younger than 0 years old")
+else:
+    "0, 1, 2, 3, 4"
+    for i in range(age):  # for i in 0, 1, 2, 3, 4
+        print(i)
+        print("Happy Birthday!")
+        print()
+
+    print("I am done! Finally!")
+
+
+"""
+range(5) -> 0, 1, 2, 3, 4
+
+2 Data Types for Numbers:
+-) Integers: 5, 6, 11, -21
+-) Floats: 0.01, -0.01, 1.0, 1.602, 0.000
+
+Data Type for Writing:
+-) Strings "Hello", "hfud", "135"
+1.0 = 1.000 = 1.00000000000
+
+Data Type for Truth:
+-) Boolean: True or False
+
+2 Loops:
+-) While Loop
+-) For Loop
+"""
