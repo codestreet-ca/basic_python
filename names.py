a9e2046572dca857c16f3851ddd086cee507eb41
Taqi Hamoda <taqihisham@gmail.com>
2021-03-14 16:29:28 -0400

    Added the sunday lesson material

---
+++ names.py
@@ -0,0 +1,4 @@
+names = ["Taqi", "Cowboy", "Ocean", "Boolean"]
+
+print(f"{name[0]} went to the store. {name[0]} saw {name[1]}.\n{name[1]} was talking to {name[3]}. {name[0]} left the store to visit {name[2]}")
+# Note: "\n" is the new line character
---
+++ story.py
@@ -0,0 +1,7 @@
+# You do not have to write this
+# If you want to write along, that is fine
+
+names = []
+for i in range(3):
+    name = input("Give me a name! ")
+    names.append(name)
